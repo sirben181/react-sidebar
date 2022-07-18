44c81ddae91df8bb7d8ed090c3c212a3fa1dd831
sirben181 <bensonkamauben@gmail.com>
2022-07-18 09:48:13 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Sidebar from "./components/Sidebar";
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="app">
+</div>
+  );
+}
+
+export default App;
